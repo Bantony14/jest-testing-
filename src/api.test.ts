146620fe8a2ec878a08fe21ed28172980386401e
Bantony <bantonysin95@gmail.com>
2026-09-14 15:59:27 +0530

    jest frotend test complete

--- src/api.test.ts
+++ src/api.test.ts
@@ -1,6 +1,11 @@
-import { expect, jest, test } from "@jest/globals";
+import { expect, jest, test, afterEach } from "@jest/globals";
 
-import { api } from "./api";
+import { api, createUser, getUser, getUserAge, twoData } from "./api";
+import axios from "axios";
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
 
 // test("should return user data", async () => {
 //   globalThis.fetch = jest
@@ -20,27 +25,125 @@ import { api } from "./api";
 //   });
 // });
 
-test("should return user data", async () => {
-  globalThis.fetch = jest
-    .fn<(input: RequestInfo | URL, init?: RequestInit) => Promise<Response>>()
-    .mockResolvedValue({
-      json: async () => ({
-        name: "bantony",
-        age: 24,
-      }),
-    } as Response);
+// test("should return user data", async () => {
+//   globalThis.fetch = jest
+//     .fn<(input: RequestInfo | URL, init?: RequestInit) => Promise<Response>>()
+//     .mockResolvedValue({
+//       json: async () => ({
+//         name: "bantony",
+//         age: 24,
+//       }),
+//     } as Response);
 
-  const user = await api();
-  expect(user).toEqual({
-    name: expect.any(String),
-    age: expect.any(Number),
-  });
+//   const user = await api();
+//   expect(user).toEqual({
+//     name: expect.any(String),
+//     age: expect.any(Number),
+//   });
+// });
+
+// test("should return user data", async () => {
+//   globalThis.fetch = jest
+//     .fn<(input: RequestInfo | URL, init?: RequestInit) => Promise<Response>>()
+//     .mockRejectedValue(new Error("Network error"));
+
+//   await expect(api()).rejects.toThrow("Network error");
+// });
+
+// test("should return user data", async () => {
+//   jest.spyOn(axios, "get").mockResolvedValue({
+//     data: {
+//       name: "bantony",
+//       age: 24,
+//     },
+//   });
+
+//   const data = await api();
+//   expect(data).toEqual({
+//     name: "bantony",
+//     age: 24,
+//   });
+// });
+
+// test("send user data", async () => {
+//  jest.spyOn(axios, "post").mockResolvedValue({
+//     data: {
+//       name: "bantony",
+//       age: 24,
+//     },
+//   });
+
+//   await createUser({
+//     name: "bantony",
+//     age: 24,
+//   });
+
+//   expect(axios.post).toHaveBeenCalledWith("/api/user", {
+//     name: "bantony",
+//     age: 24,
+//   });
+// });
+
+// test("should return user data", async () => {
+//   jest
+//     .spyOn(axios, "get")
+//     .mockResolvedValueOnce({
+//       data: {
+//         name: "bantony",
+//         age: 24,
+//       },
+//     })
+//     .mockResolvedValueOnce({
+//       data: {
+//         name: "sonu",
+//         age: 28,
+//       },
+//     })
+//     .mockRejectedValueOnce(new Error("network error"));
+
+//   const firstApi = await api();
+//   const secondApi = await api();
+
+//   expect(firstApi).toEqual({
+//     name: "bantony",
+//     age: 24,
+//   });
+
+//   expect(secondApi).toEqual({
+//     name: "sonu",
+//     age: 28,
+//   });
+
+//   await expect(api()).rejects.toThrow("network error");
+// });
+
+// test("should handle 401 error", async () => {
+//   jest.spyOn(axios, "get").mockRejectedValueOnce({
+//     response: {
+//       status: 401,
+//       data: {
+//         message: "Unauthorized",
+//       },
+//     },
+//   });
+
+//   await expect(api()).rejects.toMatchObject({
+//     response: {
+//       status: 401,
+//     },
+//   });
+// });
+
+test("retrun string", async () => {
+  await expect(getUserAge()).resolves.toBe(24);
 });
 
-test("should return user data", async () => {
-  globalThis.fetch = jest
-    .fn<(input: RequestInfo | URL, init?: RequestInit) => Promise<Response>>()
-    .mockRejectedValue(new Error("Network error"));
+test("retrun string", async () => {
+  await expect(getUser()).rejects.toThrow("User not found");
+});
 
-  await expect(api()).rejects.toThrow("Network error");
+test("retrun string", async () => {
+  const data = await twoData();
+
+  expect(data).toEqual(["ban", "tony"]);
 });
--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,42 @@
+import axios from "axios";
+
+// interface User {
+//   name: string;
+//   age: number;
+// }
+// export async function api(): Promise<User> {
+//   const response = await fetch("/api/user");
+//   return response.json();
+// }
+
 interface User {
   name: string;
   age: number;
 }
 export async function api(): Promise<User> {
-  const response = await fetch("/api/user");
-  return response.json();
+  const response = await axios.get("/api/user");
+  return response.data;
+}
+
+export async function createUser(user: User): Promise<User> {
+  const response = await axios.post("/api/user", user);
+
+  return response.data;
+}
+
+export async function getUserAge(): Promise<number> {
+  return 24;
+}
+
+export async function getUser(): Promise<string> {
+  throw new Error("User not found");
+}
+
+export async function twoData(): Promise<string[]> {
+  const [first, second] = await Promise.all([
+    Promise.resolve("ban"),
+    Promise.resolve("tony"),
+  ]);
+
+  return [first, second];
 }
